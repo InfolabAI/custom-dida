d8f3c16dbb9478e3a52c8303d83ced3263416b50
heechul <skyde1021@dgist.ac.kr>
2023-07-11 12:44:32 +0000

    edge propagation and inner augment(attention) with script_hee

--- script_hee.sh
+++ script_hee.sh
@@ -1,39 +1,23 @@
-#python main.py --model tokengt_cd --seed 123 --device_id 0 --dataset yelp --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --device_id 0 --dataset yelp --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --device_id 6 --dataset yelp --ex_name "TiaRa Augmentation" &
-#
-#python main.py --model tokengt_cd --seed 123 --device_id 1 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --device_id 1 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --device_id 1 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#
-#python main.py --model tokengt_cd --seed 123 --device_id 2 --dataset collab --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --device_id 2 --dataset collab --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --device_id 7 --dataset collab --ex_name "TiaRa Augmentation" &
-#
-#python main.py --model tokengt_cd --seed 123 --augment tiara --device_id 3 --dataset yelp --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --augment tiara --device_id 3 --dataset yelp --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --augment tiara --device_id 6 --dataset yelp --ex_name "TiaRa Augmentation" &
-#
-#python main.py --model tokengt_cd --seed 123 --augment tiara --device_id 4 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --augment tiara --device_id 4 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --augment tiara --device_id 4 --dataset bitcoin --ex_name "TiaRa Augmentation" &
-#
-#python main.py --model tokengt_cd --seed 123 --augment tiara --device_id 5 --dataset collab --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 222 --augment tiara --device_id 5 --dataset collab --ex_name "TiaRa Augmentation" &
-#python main.py --model tokengt_cd --seed 321 --augment tiara --device_id 7 --dataset collab --ex_name "TiaRa Augmentation" &
+python main.py --model tokengt_cd --seed 123 --augment edgeprop --device_id 0 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --augment edgeprop --device_id 0 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --augment edgeprop --device_id 6 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
 
-##python main.py --model dida --seed 123 --device_id 2 --dataset yelp --ex_name "TiaRa Augmentation" &
-##python main.py --model dida --seed 222 --device_id 2 --dataset yelp --ex_name "TiaRa Augmentation" &
-python main.py --model dida --seed 321 --device_id 1 --dataset yelp --ex_name "TiaRa Augmentation" &
+python main.py --model tokengt_cd --seed 123 --augment edgeprop --device_id 1 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --augment edgeprop --device_id 1 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --augment edgeprop --device_id 1 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
 
-##python main.py --model dida --seed 123 --device_id 5 --dataset collab --ex_name "TiaRa Augmentation" &
-##python main.py --model dida --seed 222 --device_id 5 --dataset collab --ex_name "TiaRa Augmentation" &
-python main.py --model dida --seed 321 --device_id 1 --dataset collab --ex_name "TiaRa Augmentation" &
+python main.py --model tokengt_cd --seed 123 --augment edgeprop --device_id 2 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --augment edgeprop --device_id 2 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --augment edgeprop --device_id 7 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
 
-##python main.py --model dida --seed 123 --augment tiara --device_id 7 --dataset yelp --ex_name "TiaRa Augmentation" &
-##python main.py --model dida --seed 222 --augment tiara --device_id 7 --dataset yelp --ex_name "TiaRa Augmentation" &
-python main.py --model dida --seed 321 --augment tiara --device_id 2 --dataset yelp --ex_name "TiaRa Augmentation" &
+python main.py --model tokengt_cd --seed 123 --hidden_augment pool --device_id 3 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --hidden_augment pool --device_id 3 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --hidden_augment pool --device_id 6 --dataset yelp --ex_name "Augmentation_test_edgeprop_poolingatt" &
 
-python main.py --model dida --seed 123 --augment tiara --device_id 4 --dataset collab --ex_name "TiaRa Augmentation" &
-python main.py --model dida --seed 222 --augment tiara --device_id 5 --dataset collab --ex_name "TiaRa Augmentation" &
-python main.py --model dida --seed 321 --augment tiara --device_id 1 --dataset collab --ex_name "TiaRa Augmentation" &
+python main.py --model tokengt_cd --seed 123 --hidden_augment pool --device_id 4 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --hidden_augment pool --device_id 4 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --hidden_augment pool --device_id 4 --dataset bitcoin --ex_name "Augmentation_test_edgeprop_poolingatt" &
+
+python main.py --model tokengt_cd --seed 123 --hidden_augment pool --device_id 5 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 222 --hidden_augment pool --device_id 5 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
+python main.py --model tokengt_cd --seed 321 --hidden_augment pool --device_id 7 --dataset collab --ex_name "Augmentation_test_edgeprop_poolingatt" &
